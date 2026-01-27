eb51e0cb1dfff2ccd183a58619dc2b01d412a2f6
Rituraj Achuthan <riturajachuthan@gmail.com>
2026-01-27 00:15:15 +0000

    feat(sketchybar): add status bar stow package
    GNU Stow-compatible sketchybar configuration with theme support:
    
    Bar styling:
    - Floating bar with rounded corners and blur
    - Dynamic colors from active theme
    - Shadow and border effects
    
    Layout:
    - Left: clock, front app with icon
    - Center: workspace indicators (1-5)
    - Right: volume, wifi, battery icons
    
    Plugins:
    - aerospace.sh: workspace focus highlighting
    - clock.sh: 24-hour time display
    - front_app.sh: active app name and icon
    - battery.sh: charge level with dynamic icon
    - volume.sh: volume level indicator
    - wifi.sh: connection status
    - icon_map_fn.sh: app icon mappings

---
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+CHARGING="$(pmset -g batt | grep 'AC Power')"
+
+if [ "$PERCENTAGE" = "" ]; then
+    exit 0
+fi
+
+# Battery icons (SF Symbols)
+case "${PERCENTAGE}" in
+    9[0-9]|100) ICON="󰁹" ;;
+    [7-8][0-9]) ICON="󰂁" ;;
+    [5-6][0-9]) ICON="󰁿" ;;
+    [3-4][0-9]) ICON="󰁽" ;;
+    [1-2][0-9]) ICON="󰁻" ;;
+    *)          ICON="󰁺" ;;
+esac
+
+# Charging icon
+if [[ "$CHARGING" != "" ]]; then
+    ICON="󰂄"
+fi
+
+sketchybar --set "$NAME" icon="$ICON"
