d8213ba3f065127f6c3c9e32dfd89fc1e59a8ba9
Rituraj Achuthan <riturajachuthan@gmail.com>
2026-01-27 12:19:43 +0000

    chore(sketchybar): clean up old items and plugins directories
    - Remove old items/ directory (superseded by modules/)
    - Remove old plugins/ directory (superseded by modules/)
    - Update .gitignore to reference utils/ instead of helpers/
    - Add native/helper binary to .gitignore
    
    All functionality now organized in modules/ directory with lib/ utilities

--- sketchybar/.config/sketchybar/plugins/front_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$SENDER" = "front_app_switched" ]; then
-  source "$HOME/.config/sketchybar/helpers/icon_map.sh"
-  __icon_map "$INFO"
-  sketchybar --set $NAME label="$INFO" icon="$icon_result"
-fi
